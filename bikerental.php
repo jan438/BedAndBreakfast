4c6d05a5c854de481321f9a0794bbbb1212005da
Jan Boon <janboon438@gmail.com>
2015-10-21 08:49:27 +0200

    Some content added to the bikerental page

--- bikerental.php
+++ bikerental.php
@@ -4,6 +4,7 @@
 
 <script type="text/javascript" src="js/opentype.min.js"></script>
 <link rel="stylesheet" href="site.css">
+<script type="text/javascript" src="js/jquery.elevatezoom.js"></script>
 
 <script type="text/javascript">
 var tr = $.tr.translator();
@@ -18,14 +19,32 @@ opentype.load('http://192.168.1.31/fonts/FiraSansMedium.woff', function(err, fon
 });
 </script>
 
-<div id="information" class="spacer reserve-info ">
+<div id="information" class="spacer reserve-info" >
 	<div class="container">
+		<div class="row">
+		<canvas id="canvas" width="500" height="75" class="text"></canvas>
+		</div> <!-- row -->
+		<div class="row">
+			<img id="zoom_01" src='images/small/fietsverhuur.png' data-zoom-image="images/large/fietsverhuur.jpg"/>
+		</div> <!-- row -->
+		<div class="row" id="de_bikerental">
+			<br>
+			<br>
+			<p>Entre campagne et lac d'Ijssel
+Zwischen Polderlandschaft und IJsselmeer
 
-	<canvas id="canvas" width="500" height="75" class="text"></canvas>
 
- 	<img src="images/large/fietsverhuur.jpg" height="190" width="445">
+Westwoud ist ein wesfriesisches Dorf in einer wunderschönen Polderlandschaft mit Haubergen, Weideland und Wassergraben. Die Umgebung ist super geeignet zum Spazieren gehen oder Fahrrad fahren. In der Umgebung ist ein Golfplatz. Museumliebhaber können Museen in den Nachbarsorten besuchen. Westwoud liegt zwischen Hoorn (7 kilometer), Enkhuizen (12 kilometer) und Medemblik (12 kilometer): drei schöne Hafenstädte am IJsselmeer.</p>
 
+		</div>	<!-- row -->
 	</div> <!-- container -->
 </div> <!-- information -->
-
+<script>
+    $('#zoom_01').elevateZoom({
+	zoomType: "inner",
+	cursor: "crosshair",
+	zoomWindowFadeIn: 500,
+	zoomWindowFadeOut: 750
+   }); 
+</script>
 <?php include 'footer.php';?>
