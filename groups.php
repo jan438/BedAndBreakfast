8d94d87f10d2ccf032675a1cd4b9e1e4aaf54a77
Jan Boon <janboon438@gmail.com>
2015-10-24 11:13:20 +0200

    A rolling effect applied

--- groups.php
+++ groups.php
@@ -30,7 +30,7 @@ opentype.load('http://192.168.1.31/fonts/FiraSansMedium.woff', function(err, fon
 			<img src="images/photos/gallery/51-Oosterblokker op terras.jpg" alt="Groups" height="322" width="430">
 		</div> <!-- row -->
 
-            <div data-lining class="poem preview">
+            <div data-lining data-auto-resize class="poem voffset4" data-effect="rolling">
                 Reünie of Teamwork. De sfeervolle eetzaal biedt ruimte voor 20 personen. De comfortabele armstoelen geven de mogelijkheid om uren lang te vergaderen of gewoon gezellig samen zitten.<br/>Uw gastvrouw kan voor koffie en hapjes zorgen, van gewoon kopje koffie, tot uitgebreide buffet en luxe toastjes.<br/>Tv, dvd en muziekinstallatie zijn er aanwezig. Op aanvraag kunt u van een flipover gebruik maken..<br/>De pui van de eetzaal kan volledig open gaan en het  100m2 groot terras die er aansluit biedt ruimte voor ongeveer 50 mensen.<br/>Bij mooi weer is er de gelegenheid om een tuinfeest en BBQ te organiseren.<br/>
             </div>
 	</div> <!-- container -->
